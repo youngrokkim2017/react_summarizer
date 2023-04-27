38ea09c2206caae62d7eb551a823290393ba2cb8
Patrick Kim <patrick.yrkim@gmail.com>
2023-04-27 14:43:07 -0700

    Add Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { logo } from '../assets'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt="some_logo" className='w-28 object-contain' />
+
+        {/* button to github repo */}
+        {/* <button type='button' onClick={() => {}} className='black_btn>
+          GitHub
+        </button> */}
+      </nav>
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAI GPT</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify articles
+      </h2>
+    </header>
   )
 }
 
